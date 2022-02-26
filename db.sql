322f66bf76ceb89fff1153d696b97904d1509eff
tmedcount <tmedcount@gmail.com>
2022-02-26 18:04:29 +0900

    DB 생성, 게시물 테이블 생성

---
+++ db.sql
@@ -0,0 +1,44 @@
+#DB 생성
+DROP DATABASE IF EXISTS tableBoard;
+CREATE DATABASE tableBoard;
+USE tableBoard;
+
+#게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL
+);
+
+DESC article;
+
+#게시물 데이터 3개 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1',
+memberId = 1,
+boardId = 1;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2',
+memberId = 1,
+boardId = 1;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+memberId = 1,
+boardId = 1;
+
+SELECT * FROM article;
