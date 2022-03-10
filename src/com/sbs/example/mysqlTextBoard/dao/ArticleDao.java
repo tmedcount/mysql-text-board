506c26577580066b731b28c3b409d76b0175d00a
tmedcount <tmedcount@gmail.com>
2022-03-10 17:18:57 +0900

    ArticleDao의 add에 MySQLUtil 적용

--- src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
+++ src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
@@ -1,11 +1,5 @@
 package com.sbs.example.mysqlTextBoard.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -52,64 +46,18 @@ public class ArticleDao {
 	}
 
 	public int add(int boardId, int memberId, String title, String body) {
-		int id = 0;
-		Connection con = null;
 		
-		try {
-			
-			String dbmsJdbcUrl = "jdbc:mysql://127.0.0.1:3306/textBoard?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000&socketTimeout=60000";
-			String dbmsLoginId = "sbsst";
-			String dbmsLoginPw = "sbs123414";
-			
-			// 기사 등록
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			
-			// 연결 생성
-			try {
-				con = DriverManager.getConnection(dbmsJdbcUrl, dbmsLoginId, dbmsLoginPw);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-			String sql = "INSERT INTO article";
-			sql += " SET regDate = NOW(),";
-			sql += " updateDate = NOW(),";
-			sql += " boardId = ?,";
-			sql += " memberId = ?,";
-			sql += " title = ?,";
-			sql += " body = ?";
-			
-			try {
-				PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-				pstmt.setInt(1, boardId);
-				pstmt.setInt(2, memberId);
-				pstmt.setString(3, title);
-				pstmt.setString(4, body);
-				
-				pstmt.executeUpdate();
-				
-				ResultSet rs = pstmt.getGeneratedKeys();
-				rs.next();
-				id = rs.getInt(1);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
-		} finally {
-			try {
-				// System.out.println("여기는 항상 실행 됨!!!!!!!!!!!!!!!!");
-				if(con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
 		
-		return id;
+		SecSql sql = new SecSql();
+		
+		sql.append("INSERT INTO article");
+		sql.append(" SET regDate = NOW(),");
+		sql.append(" updateDate = NOW(),");
+		sql.append( " boardId = ?,", boardId);
+		sql.append( " memberId = ?,", memberId);
+		sql.append(" title = ?,", title);
+		sql.append(" body = ?", body);
+
+		return MysqlUtil.insert(sql);
 	}
 }
