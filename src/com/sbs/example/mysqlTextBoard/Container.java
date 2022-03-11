0a0a5ef05cf8fdfa27abedb007f038ba013784cc
tmedcount <tmedcount@gmail.com>
2022-03-11 16:15:19 +0900

    서비스를 컨테이너에서 공급받도록 구조 수정

--- src/com/sbs/example/mysqlTextBoard/Container.java
+++ src/com/sbs/example/mysqlTextBoard/Container.java
@@ -2,12 +2,20 @@ package com.sbs.example.mysqlTextBoard;
 
 import java.util.Scanner;
 
+import com.sbs.example.mysqlTextBoard.service.ArticleService;
+import com.sbs.example.mysqlTextBoard.service.MemberService;
+
 public class Container {
 
 	public static Scanner scanner;
+	public static MemberService memberService;
+	public static ArticleService articleService;
 	
 	static {
 		scanner = new Scanner(System.in);
+		
+		memberService = new MemberService();
+		articleService = new ArticleService();
 	}
 
 }
--- src/com/sbs/example/mysqlTextBoard/controller/MemberController.java
+++ src/com/sbs/example/mysqlTextBoard/controller/MemberController.java
@@ -10,7 +10,7 @@ public class MemberController {
 	private MemberService memberService;
 	
 	public MemberController() {
-		memberService = new MemberService();
+		memberService = Container.memberService;
 	}
 
 	public void doCommand(String cmd) {
