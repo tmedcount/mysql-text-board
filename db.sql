8956675584ecb638cd388bdf78662ab394149bc5
tmedcount <tmedcount@gmail.com>
2022-04-01 21:48:00 +0900

    게시물 상세페이지에 토스트UI뷰어 적용

--- db.sql
+++ db.sql
@@ -14,7 +14,7 @@ CREATE TABLE article (
     boardId INT(10) UNSIGNED NOT NULL
 );
 
-# 게시물 데이터 3개`tableBoard` 생성
+# 게시물 데이터 3개 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
@@ -40,13 +40,13 @@ memberId = 1,
 boardId = 1;
 
 # 회원 테이블 생성
-CREATE TABLE `member`(
+CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
     loginId CHAR(30) NOT NULL,
     loginPw VARCHAR(50) NOT NULL,
-    `name` CHAR(10) NOT NULL
+    `name` CHAR(30) NOT NULL
 );
 
 # 회원 데이터 생성
@@ -65,7 +65,7 @@ loginPw = 'test2',
 `name` = '테스터2';
 
 # 게시판 테이블 생성
-CREATE TABLE board(
+CREATE TABLE board (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
@@ -74,26 +74,288 @@ CREATE TABLE board(
 );
 
 # 공지사항 게시판 추가
-INSERT INTO board
+INSERT INTO board 
 SET regDate = NOW(),
 updateDate = NOW(),
 `name` = '공지사항',
 `code` = 'notice';
 
 # 자유 게시판 추가
-INSERT INTO board
+INSERT INTO board 
 SET regDate = NOW(),
 updateDate = NOW(),
 `name` = '자유',
 `code` = 'free';
 
+UPDATE article
+SET boardId = 2
+LIMIT 2;
+
+# 각종 함수
+/*
+SELECT DATE(NOW());
+SELECT YEAR(NOW());
+SELECT MONTH(NOW());
+SELECT DAY(NOW());
+SELECT SUBSTR("안녕하세요.", 1, 2);
+SELECT SUBSTR("안녕하세요.", 2, 2);
+SELECT CONCAT("안녕", "하세요.");
+SELECT RAND() * 100;
+*/
+
 # 게시물 랜덤 생성
-SELECT CONCAT("제목_", RAND());
+/*
+insert into article
+set regDate = NOW(),
+updateDate = NOW(),
+title = concat("제목_", rand()),
+`body` = CONCAT("내용_", RAND()),
+memberId = FLOOR(RAND() * 2) + 1,
+boardId = FLOOR(RAND() * 2) + 1;
+*/
+
+# 3번글 내용을 마크다운 문법으로 수정
+UPDATE article SET `body` = '#공지사항\r\n안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C'
+WHERE id = '3'; 
+
+SELECT * FROM article;# DB 생성
+DROP DATABASE IF EXISTS textBoard;
+CREATE DATABASE textBoard;
+USE textBoard;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL
+);
+
+# 게시물 데이터 3개 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1',
+memberId = 1,
+boardId = 1;
 
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
-title = CONCAT("제목_", RAND()),
+title = '제목2',
+`body` = '내용2',
+memberId = 1,
+boardId = 1;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+memberId = 1,
+boardId = 1;
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(30) NOT NULL,
+    loginPw VARCHAR(50) NOT NULL,
+    `name` CHAR(30) NOT NULL
+);
+
+# 회원 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '테스터1';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '테스터2';
+
+# 게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    `code` CHAR(20) NOT NULL
+);
+
+# 공지사항 게시판 추가
+INSERT INTO board 
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '공지사항',
+`code` = 'notice';
+
+# 자유 게시판 추가
+INSERT INTO board 
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '자유',
+`code` = 'free';
+
+UPDATE article
+SET boardId = 2
+LIMIT 2;
+
+# 각종 함수
+/*
+SELECT DATE(NOW());
+SELECT YEAR(NOW());
+SELECT MONTH(NOW());
+SELECT DAY(NOW());
+SELECT SUBSTR("안녕하세요.", 1, 2);
+SELECT SUBSTR("안녕하세요.", 2, 2);
+SELECT CONCAT("안녕", "하세요.");
+SELECT RAND() * 100;
+*/
+
+# 게시물 랜덤 생성
+/*
+insert into article
+set regDate = NOW(),
+updateDate = NOW(),
+title = concat("제목_", rand()),
 `body` = CONCAT("내용_", RAND()),
 memberId = FLOOR(RAND() * 2) + 1,
-boardId = FLOOR(RAND() * 2) + 1
+boardId = FLOOR(RAND() * 2) + 1;
+*/
+
+# 3번글 내용을 마크다운 문법으로 수정
+UPDATE article SET `body` = '#공지사항\r\n안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C'
+WHERE id = '3'; 
+
+SELECT * FROM article;# DB 생성
+DROP DATABASE IF EXISTS textBoard;
+CREATE DATABASE textBoard;
+USE textBoard;
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
+    `body` TEXT NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL
+);
+
+# 게시물 데이터 3개 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1',
+memberId = 1,
+boardId = 1;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2',
+memberId = 1,
+boardId = 1;
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3',
+memberId = 1,
+boardId = 1;
+
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(30) NOT NULL,
+    loginPw VARCHAR(50) NOT NULL,
+    `name` CHAR(30) NOT NULL
+);
+
+# 회원 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '테스터1';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '테스터2';
+
+# 게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    `code` CHAR(20) NOT NULL
+);
+
+# 공지사항 게시판 추가
+INSERT INTO board 
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '공지사항',
+`code` = 'notice';
+
+# 자유 게시판 추가
+INSERT INTO board 
+SET regDate = NOW(),
+updateDate = NOW(),
+`name` = '자유',
+`code` = 'free';
+
+UPDATE article
+SET boardId = 2
+LIMIT 2;
+
+# 각종 함수
+/*
+SELECT DATE(NOW());
+SELECT YEAR(NOW());
+SELECT MONTH(NOW());
+SELECT DAY(NOW());
+SELECT SUBSTR("안녕하세요.", 1, 2);
+SELECT SUBSTR("안녕하세요.", 2, 2);
+SELECT CONCAT("안녕", "하세요.");
+SELECT RAND() * 100;
+*/
+
+# 게시물 랜덤 생성
+/*
+insert into article
+set regDate = NOW(),
+updateDate = NOW(),
+title = concat("제목_", rand()),
+`body` = CONCAT("내용_", RAND()),
+memberId = FLOOR(RAND() * 2) + 1,
+boardId = FLOOR(RAND() * 2) + 1;
+*/
+
+# 3번글 내용을 마크다운 문법으로 수정
+UPDATE article SET `body` = '#공지사항\r\n안녕하세요.\r\n이 사이트는 저의 글 연재 공간입니다.\r\n\r\n---\r\n\r\n# 이 사이트의 특징\r\n- A\r\n- B\r\n- C'
+WHERE id = '3'; 
+
+SELECT * FROM article;
