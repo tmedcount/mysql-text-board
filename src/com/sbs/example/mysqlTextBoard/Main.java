fa167f6887e85625bd07fc38d340719106028545
tmedcount <tmedcount@gmail.com>
2022-04-12 15:48:37 +0900

    HTTP REQUEST 후 응답 받아오는 함수 구현

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,10 +1,21 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.util.Util;
+
 public class Main {
 
 	public static void main(String[] args) {
+		testApi();		
 		
-		new App().run();
+		// new App().run();
+	}
+
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+
+		String rs = Util.callApi(url, "api_key=7etmI9A5O6B4mJ1AoakRWtHUchhmGkNvmUjc0Jb4CnEy367qo67hZg1Rm2jBznEq", "forum=yamto", "thread:ident=article_detail_1.html");
+		
+		System.out.println(rs);
 	}
 
 }
